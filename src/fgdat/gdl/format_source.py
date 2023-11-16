32c9c5f79f791827e5744a79de941e9afd2a085e
xiaokaizhang <xiaokaizhang1999@163.com>
2023-11-16 11:40:59 +0800

    format gdl

--- src/fgdat/gdl/format_source.py
+++ src/fgdat/gdl/format_source.py
@@ -20,8 +20,8 @@ def format_predicate_gdl_source(input_filename, output_filename):
                 "doc_cn": data["Predicates"][p_class][p_name]["doc_cn"],
                 "doc_en": "",
                 "pic_width": data["Predicates"][p_class][p_name]["pic_width"],
-                "anti_parse_to_nl_cn": "",
-                "anti_parse_to_nl_en": "",
+                "anti_parse_to_nl_cn": [],
+                "anti_parse_to_nl_en": [],
             }
 
     save_json(new_data, output_filename)
@@ -49,5 +49,5 @@ def format_theorem_gdl_source(input_filename, output_filename):
 if __name__ == '__main__':
     format_predicate_gdl_source("../../../projects/formalgeo7k/files/predicate_GDL-source.json",
                                 "../../../projects/formalgeo7k/files/predicate_GDL-source-format.json")
-    format_theorem_gdl_source("../../../projects/formalgeo7k/files/theorem_GDL-source.json",
-                              "../../../projects/formalgeo7k/files/theorem_GDL-source-format.json")
+    # format_theorem_gdl_source("../../../projects/formalgeo7k/files/theorem_GDL-source.json",
+    #                           "../../../projects/formalgeo7k/files/theorem_GDL-source-format.json")
